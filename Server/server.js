1b312fc6f2b3ae76f3e21d64a2babaa71f2bcecd
GaneshramWebdev <gram00552@gmail.com>
2025-04-24 19:47:45 +0530

    local

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://jwt-auth-2-z680.onrender.com",
+    origin: ["https://jwt-auth-2-z680.onrender.com", "http://localhost:3000"],
     credentials: true,
   })
 );
